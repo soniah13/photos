b5595701dfac24aadfd98aee65dd0b2388e67aa5
imorgansonia <imorgansonia@gmail.com>
2024-10-09 22:57:08 +0300

    third commit

--- PhotoGallery/settings.py
+++ PhotoGallery/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOST', ''). split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOST', 'photo-cy6d.onrender.com'). split(',')
 
 cloudinary.config(
   cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'),
